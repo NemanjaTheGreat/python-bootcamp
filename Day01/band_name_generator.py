867439b534709ee710bdf493639b04b6b564222d
nemanja_perovic <n.perovic@futureforward.rs:git config --global user.email n.perovic@futureforward.rs>
2026-07-13 17:36:09 +0800

    Day 1 - Band Name Generator

---
+++ Day01/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city_name = input("Which city did you grew up in? : ")
+pet_name = input("What's your pet's name? : ")
+print("Your band name could be " + city_name + " " + pet_name)
