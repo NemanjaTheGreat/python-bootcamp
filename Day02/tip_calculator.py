174c8f9ae5f867c9ecf08bc850f0f796a19583ea
nemanja_perovic <n.perovic@futureforward.rs:git config --global user.email n.perovic@futureforward.rs>
2026-07-15 01:22:11 +0800

    Improve Day 2 formatting

--- Day02/tip_calculator.py
+++ Day02/tip_calculator.py
@@ -1,7 +1,7 @@
 print("Welcome to the tip calculator!")
-total_bill = float(input("What was the total bill? "))
-tip = int (input("How much tip would you like to give, 10, 12 or 15? :"))
-people_number = int (input("How many people to split the bill? :"))
+total_bill = float(input("What was the total bill?: "))
+tip = int (input("How much tip would you like to give, 10, 12 or 15?: "))
+people_number = int (input("How many people to split the bill?: "))
 tip_amount = total_bill * (tip / 100)
 final_bill = total_bill + tip_amount
 pay_per_person = (final_bill / people_number)
